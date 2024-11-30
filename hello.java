b0c7c8a6024ffd558b258e179217492e5e621066
sanjeevveerapandian <115895064+sanjeevveerapandian@users.noreply.github.com>
2024-12-01 01:02:00 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,33 @@
+class hello{
+  public static void main(String args[])
+  {
+    System.out.print("Hello World");
+  }
+}
+
+/*english -> bite code -> jvm code -> machine readable code  */
+
+
+
+// Variables and DataTypes
+
+ class hello{
+ public static void main(String args[])
+ {
+ String container = "fish";
+ int box = 67;
+System.out.println(container);
+System.out.println(box);
+ }
+}
+ 
+
+class hello{
+  public static void main(String args[])
+  {
+    int a = 80;
+    int b = 20;
+    int c = 10;
+    System.out.println(a + b - c);
+  }
+}
