43548b6ebc49c9160580a7fb6973da4edebf7c6b
sanjeevveerapandian <115895064+sanjeevveerapandian@users.noreply.github.com>
2024-12-03 02:00:17 +0530

    second day

---
+++ q1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class q1{
+  public static void main(String args[])
+  {
+   Scanner scan = new Scanner(System.in);
+   String name = scan.nextLine();
+   int age = scan.nextInt();
+   System.out.println("my name is "+name);
+   System.out.print("my age is "+age);
+  }
+}
---
+++ q4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class q4{
+  public static void main(String args[])
+  {
+    Scanner scan = new Scanner(System.in);
+    String name = scan.nextLine();
+    int score = scan.nextInt();
+    scan.nextLine();
+    String department =scan.nextLine();
+
+    System.out.println("my name is "+name);
+    System.out.println("my score is "+score +"%");
+    System.out.println("my dept is "+department);
+
+  }
+}
