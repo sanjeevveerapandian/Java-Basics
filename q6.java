2eda8fe3fb2cc469d80530d4ab8afb678cd1ed25
sanjeevveerapandian <115895064+sanjeevveerapandian@users.noreply.github.com>
2024-12-03 23:59:18 +0530

    commit

---
+++ q6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class demo{
+public static void main(String args[]){
+// Check whether Number 1 and Number 2 is Equal or not I
+Scanner scan = new Scanner(System.in);
+int num1 = scan.nextInt() ;
+int num2 = scan.nextInt();
+
+if (num1==num2) 
+{ 
+System.out.println("they are equal");
+} 
+else{
+System.out.print("Not Equal");
+} 
+}
+}
