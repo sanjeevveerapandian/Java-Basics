85315b29f6e509e32de2de0292eab1b41f0af380
sanjeevveerapandian <115895064+sanjeevveerapandian@users.noreply.github.com>
2024-12-03 23:50:18 +0530

    commit

---
+++ q5.java
@@ -0,0 +1,9 @@
+class demo{
+  public static void main(String args[])
+  {
+  // Check whether Number 1 and Number 2 is Equal or not
+  int num1 = 20;
+  int num2 = 70;
+  System.out.print(3==3);
+  }
+  }
