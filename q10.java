3ee5af0f01684cd35291f3e8a600e5dd88e810e2
sanjeevveerapandian <115895064+sanjeevveerapandian@users.noreply.github.com>
2024-12-12 20:50:59 +0530

    update

---
+++ q10.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+class Hello{
+	public static void main(String args[])
+	{
+	String RCB="win";
+	
+	if (RCB == "win")
+	{
+	System.out.println("Ee sala cup namadhe");
+	}
+	else
+	{
+	System.out.println("Cup illa");
+	}
+	}
+	}
