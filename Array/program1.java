c4db2323d21f48bbf286534f8e2924bfcb1f7e39
sanjeevveerapandian <115895064+sanjeevveerapandian@users.noreply.github.com>
2025-05-12 23:28:54 +0530

    strive starting

--- Array/program1.java
+++
@@ -1,7 +0,0 @@
-public class program1 {
-  public static void main(String[] args){
-    int[] a={10,20,30};
-    System.out.println(a);
-    System.out.println(a.length);
-  }
-}
--- Loops/PrintNumbers1to5.java
+++
@@ -1,9 +0,0 @@
-package Loops;
-
-public class PrintNumbers1to5 {
-  public static void main(String[] args) {
-    for (int i = 1; i <= 5; i++) {
-        System.out.println(i);
-    }
-}
-}
